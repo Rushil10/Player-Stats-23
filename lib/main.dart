64cef96cc6359437d43388d2b500c6824a0daa43
Rushil10 <rushil.10.shah@gmail.com>
2023-02-05 10:35:28 +0530

    subtle

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,8 @@ import 'package:player_stats_23/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-//import 'package:native_ads_example/native_ad_view_wrapper.dart';
 
 void main() {
-  //List<String> testDeviceIds = ['E25EE02DA7817F620865558627D4F3E0'];
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
       overlays: [SystemUiOverlay.bottom, SystemUiOverlay.top]);
@@ -27,24 +25,5 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: const SetUpLocalDb(),
     );
-    /* return FutureBuilder(
-      future: Init.instance.initialize(),
-      builder: (context, AsyncSnapshot snapshot) {
-        // Show splash screhile waiting for app resources to load:
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const MaterialApp(home: Splash());
-        } else {
-          // Loading is done, return the app:
-          return MaterialApp(
-            title: 'Fifa Stats',
-            themeMode: ThemeMode.dark,
-            darkTheme: ThemeClass.darkTheme,
-            theme: ThemeClass.lightTheme,
-            debugShowCheckedModeBanner: false,
-            home: SetUpLocalDb(),
-          );
-        }
-      },
-    ); */
   }
 }
